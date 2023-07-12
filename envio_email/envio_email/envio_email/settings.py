86b429fffe4b925abf493ef1de431e64d812cd0c
Jean <jcharaco@unsa.edu.pe>
2023-07-11 20:36:52 -0500

    añadido variables para envio de email en settings.py

--- envio_email/envio_email/envio_email/settings.py
+++ envio_email/envio_email/envio_email/settings.py
@@ -118,6 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'jean13@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
