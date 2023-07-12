4815d88484373f94479cdfabc8e2b8c85f9b994e
Jean <jcharaco@unsa.edu.pe>
2023-07-11 20:57:07 -0500

    correcion en settings y url

--- envio_email/envio_email/send/views.py
+++ envio_email/envio_email/send/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 
 def index(request):
+    
+    send_mail('Hello from Django',
+    'Hello there. This is an automated message.',
+    'jean13@gmail.com',
+    ['jcharaco@unsa.edu.pe'],
+    fail_silently=False)
 
     return render(request, 'send/index.html')
