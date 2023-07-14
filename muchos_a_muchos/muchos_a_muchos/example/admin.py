901e1d850d7e066c49f804745502411856f27c47
Jean <jcharaco@unsa.edu.pe>
2023-07-13 19:45:40 -0500

    correccion muchos a muchos añadido a admin

--- muchos_a_muchos/muchos_a_muchos/example/admin.py
+++ muchos_a_muchos/muchos_a_muchos/example/admin.py
@@ -1,3 +1,7 @@
+from xml.dom.minidom import CharacterData
 from django.contrib import admin
-
+from .models import Movie,Character
 # Register your models here.
+
+admin.site.register(Character)
+admin.site.register(Movie)
