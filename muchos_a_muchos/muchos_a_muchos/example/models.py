5f09d48160e72949747ab901d264b81c4b5fa859
Jean <jcharaco@unsa.edu.pe>
2023-07-13 19:56:15 -0500

    añadido str para lenguaje y framework en uno a muchos

--- muchos_a_muchos/muchos_a_muchos/example/models.py
+++ muchos_a_muchos/muchos_a_muchos/example/models.py
@@ -28,13 +28,13 @@ class Framework(models.Model):
         return self.name
 
 class Movie(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Character(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
     movies = models.ManyToManyField(Movie)
 
     def __str__(self):
